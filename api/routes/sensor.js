bd33194d6a0ca5fa090dea8dc7ad84318fece85e
federai <69877783+federai@users.noreply.github.com>
2021-06-08 12:19:27 -0300

    connection to mongoose server

--- api/routes/sensor.js
+++ api/routes/sensor.js
@@ -1,4 +1,29 @@
 const express = require ('express');
 const router = express.Router();
 
-router.get()
+router.get('/',(req,res,next)=>{
+    res.status(200).json({
+        message: 'Handling / request'
+    });
+});
+
+
+router.post('/',(req,res,next)=>{
+    res.status(200).json({
+        message: 'Handling / Post request'
+    });
+});
+
+router.patch('/:sensorId',(req,res,next)=>{
+    res.status(200).json({
+        message:'updated product'
+    });
+});
+
+router.delete('/:sensorId',(req,res,next)=>{
+    res.status(200).json({
+        message:'updated product'
+    });
+});
+
+module.exports = router;
